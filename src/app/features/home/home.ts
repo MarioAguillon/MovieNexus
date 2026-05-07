aa5d94293e1c206ec5577881188c7d384ba7e2e2
MarioAguillon <aguillondavis@gmail.com>
2026-05-07 15:11:33 -0500

    feat: implementar componente Hero con datos dinámicos de la API

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,23 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MovieService } from '../../core/services/movie.service';
+import { Hero } from './components/hero/hero'; // ¡Importamos el Hero!
+import { Movie } from '../../core/models/movie.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, Hero], // Lo añadimos aquí
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
   private movieService = inject(MovieService);
 
+  // Usamos una Signal para guardar la película destacada
+  featuredMovie = signal<Movie | null>(null);
+
   ngOnInit(): void {
-    console.log('🏠 Home inicializado. Cargando películas...');
     this.movieService.getTrendingMovies().subscribe({
       next: (data) => {
-        console.log('🎬 Datos recibidos de TMDB:', data.results);
-      },
-      error: (err) => {
-        console.error('❌ Error al conectar con TMDB:', err);
+        if (data.results.length > 0) {
+          // Tomamos la posición [0] del array para ser el Hero
+          this.featuredMovie.set(data.results[0]);
+        }
       }
     });
   }
